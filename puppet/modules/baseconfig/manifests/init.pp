17dcf32dffcb68161368fcbca33e1d5d2d083e57
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-10-29 11:13:39 +0000

    Switch to use heira and packaged puppet
    So HMRC, IDA and ERTP have used this pattern successfully for a while.
    We package up puppet into a tarfile, complete with all dependencies
    (Except ruby of course), and at vagrant up time we simply extract and
    run the zip.
    Actually it's a bit more complex than this because we do the package
    step at vagrant up time, but in the future we could do that on a CI box
    if we want.
    
    We're also now using Hiera to control puppet, which will start to make
    life easier.  I've left in the requirement for hiera-gpg but we're not
    using it yet because we won't need it until we start actually
    provisioning machines.

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -1,19 +1,10 @@
-# basic setup - update apt-get and move os level configs into place
 
 class baseconfig {
-    exec { 'apt-get update':
-        command => '/usr/bin/apt-get update';
-    }
-
     file { '/home/vagrant/.bashrc':
         owner   => 'vagrant',
         group   => 'vagrant',
         mode    => '0644',
         source  => 'puppet:///modules/baseconfig/bashrc';
-    }
-
-    package { 'make':
-        ensure => present;
     }
 }
 
