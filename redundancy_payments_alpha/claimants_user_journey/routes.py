7e88f525ebc2f5838675105c6ed024ae9c841de2
Gareth <grogers@thoughtworks.com>
2013-10-31 09:23:44 +0000

    Print json summary in the done page

--- redundancy_payments_alpha/claimants_user_journey/routes.py
+++ redundancy_payments_alpha/claimants_user_journey/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+import json
+from flask import Flask, render_template, url_for, session
 from werkzeug.utils import redirect
 from forms.claimant_contact_details import ClaimantContactDetails
 
@@ -17,10 +18,12 @@ def status():
 def claimant_contact_details():
     form = ClaimantContactDetails()
     if form.validate_on_submit():
+        session['user_details'] = form.data
         return redirect(url_for('done'))
     return render_template('user_details.html', form=form)
 
 
 @app.route('/done', methods=['GET'])
 def done():
-    return render_template('done.html')
+    user_details_json = json.dumps(session.get('user_details'))
+    return render_template('done.html', user_details=user_details_json)
