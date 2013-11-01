d928b8d40606b3ea5b369ede258803ab3b2b0116
rodcarter <rod.carter@insolvency.gsi.gov.uk>
2013-11-01 16:33:55 +0000

    Added additional validation to the employment details

--- redundancy_payments_alpha/claimants_user_journey/forms/employment_details.py
+++ redundancy_payments_alpha/claimants_user_journey/forms/employment_details.py
@@ -1,7 +1,25 @@
 import re
+from datetime import date
 from flask_wtf import Form
-from wtforms import TextField, SelectField
+from wtforms import TextField, SelectField, ValidationError
 from wtforms.validators import DataRequired, Length, AnyOf, Regexp
+from claimant_contact_details import convert_string_to_date
+
+
+class FutureDateValidator(Regexp):
+    def __init__(self, format_message, future_message):
+        self.future_message = future_message
+        super(FutureDateValidator, self).__init__(r'^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$', re.IGNORECASE, format_message)
+
+    def __call__(self, form, field):
+        message = self.message
+        super(FutureDateValidator, self).__call__(form, field, message)
+        extracted_date = convert_string_to_date(field.data)
+        if self.future_message is None:
+            self.future_message = 'Date cannot be in the future'
+
+        if extracted_date > date.today():
+            raise ValidationError(self.future_message)
 
 
 class EmploymentDetails(Form):
@@ -9,8 +27,7 @@ class EmploymentDetails(Form):
     category_of_worker = SelectField('Category of Worker',
                                      choices=[
                                          ('Employed', 'Employed'),
-                                         ('Labour-only', 'Labour-only'),
-                                         ('Sub-contractor', 'Sub-contractor'),
+                                         ('Labour-only Sub-contractor', 'Labour-only Sub-contractor'),
                                          ('Agency Worker', 'Agency Worker'),
 									     ('Fixed-term contracts worker', 'Fixed-term contracts worker'),
                                          ('Director or Shareholder', 'Director or Shareholder'),
@@ -22,8 +39,7 @@ class EmploymentDetails(Form):
                                      default='',
                                      validators=[AnyOf(values=[
                                          'Employed',
-                                         'Labour-only',
-                                         'Sub-contractor',
+                                         'Labour-only Sub-contractor',
                                          'Agency Worker',
                                          'Fixed-term contracts work',
                                          'Director or Shareholder',
@@ -33,11 +49,10 @@ class EmploymentDetails(Form):
                                      ])])
     start_date = TextField('When did you start working for this employer?',
                            validators=[DataRequired(),
-                                       Regexp(regex=re.compile('^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$'),
-                                        message="Date must be in the format dd/mm/yyyy.")])
+                                       FutureDateValidator(format_message="Start date must be in the format dd/mm/yyyy.",
+                                                           future_message='Start date cannot be in the future.')])
     end_date = TextField('When did your employment end?',
                            validators=[DataRequired(),
-                                       Regexp(regex=re.compile('^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$'),
-                                        message="Date must be in the format dd/mm/yyyy.")])
-
+                                       FutureDateValidator(format_message="End date must be in the format dd/mm/yyyy.",
+                                                           future_message='End date cannot be in the future.')])
 
