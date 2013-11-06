e3fb01d605ea630ec40f944d18301a540552b1f8
Andy Price <sixdaysandy@gmail.com>
2013-11-06 13:50:25 +0000

    Speed up teardown

--- birmingham_cabinet/feature_tests/environment.py
+++ birmingham_cabinet/feature_tests/environment.py
@@ -1,6 +1,10 @@
-from birmingham_cabinet.models import *
+import contextlib
+
 from birmingham_cabinet.base import Base, local_unix_socket_engine
 
 def after_feature(context, feature):
-    Base.metadata.drop_all(local_unix_socket_engine)
-    Base.metadata.create_all(local_unix_socket_engine)
+    with contextlib.closing(local_unix_socket_engine.connect()) as conn:
+        trans = conn.begin()
+        for table in reversed(Base.metadata.sorted_tables):
+            conn.execute("truncate table {table_name}".format(table_name=table.name))
+        trans.commit()
