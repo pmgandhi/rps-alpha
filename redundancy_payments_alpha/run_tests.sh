9c8855c312e384f921daed7d73af451d7ce1374d
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-11-06 18:38:20 +0000

    Add a check for VirtualEnv and run pip

--- redundancy_payments_alpha/run_tests.sh
+++ redundancy_payments_alpha/run_tests.sh
@@ -4,6 +4,19 @@ set -oe pipefail
 
 PATH=$PATH:$(pwd)
 
+if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
+  basedir=$(dirname $0)
+  venvdir=$WORKON_HOME/$(basename $(cd $(dirname $0) && pwd -P))
+
+  if [ ! -d "$venvdir" ]; then
+    virtualenv $venvdir
+  fi
+
+  source "$venvdir/bin/activate"
+
+  pip install -r requirements.dev.txt
+fi
+
 nosetests --exe
 
 behave -q --tags=-wip --stop feature_tests/
