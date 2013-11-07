d71f3cb708be738bcbd7d364ec121bbcf891a8cc
Andy Price <sixdaysandy@gmail.com>
2013-11-07 10:48:50 +0000

    Make localdev use postgres
    - Put HSTORE into rps_alpha, not template1

--- puppet/modules/rps_postgres/manifests/hstore.pp
+++ puppet/modules/rps_postgres/manifests/hstore.pp
@@ -1,8 +1,8 @@
 class rps_postgres::hstore {
     class {'postgresql::server::contrib': }
 
-    exec {'add hstore to the template database':
-        command => "/usr/bin/psql template1 -c 'create extension if not exists hstore;'",
+    exec {'add hstore to the rps_alpha database':
+        command => "/usr/bin/psql rps_alpha -c 'create extension if not exists hstore;'",
         user    => 'postgres',
         require => Class['postgresql::server::contrib']
     }
