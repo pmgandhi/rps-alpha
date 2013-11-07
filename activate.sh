1018cb5399576b5eaaef5810090746092ccd0e20
Andy Price <sixdaysandy@gmail.com>
2013-11-07 12:23:00 +0000

    Fix up jenkins.sh and activate.sh

--- activate.sh
+++ activate.sh
@@ -14,5 +14,9 @@ if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
 
    # meh - see https://github.com/pypa/pip/issues/56
    pip install -r redundancy_payments_alpha/requirements.dev.txt
+   pip install -r birmingham_cabinet/requirements.dev.txt
 
+   cd birmingham_cabinet
+   ./ensure_clean_tables
+   cd ..
 fi
