c6ad17a2dc9e735c5526f28c9323bb8839595b5c
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2013-11-07 10:25:20 +0000

    Remove birmingham cabinet requirements until puppet dependency is merged

--- activate.sh
+++ activate.sh
@@ -14,6 +14,5 @@ if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
 
    # meh - see https://github.com/pypa/pip/issues/56
    pip install -r redundancy_payments_alpha/requirements.dev.txt
-   pip install -r birmingham_cabinet/requirements.dev.txt
 
 fi
