e684c27e463cb6ad18cbbe988d6edb7936fd983d
Cal Paterson <cpaterso@thoughtworks.com>
2013-11-08 11:57:15 +0000

    Remove birmingham cabinet requirements file

--- jenkins.sh
+++ jenkins.sh
@@ -18,7 +18,6 @@ if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
 
   # meh - see https://github.com/pypa/pip/issues/56
   pip install -r redundancy_payments_alpha/requirements.dev.txt
-  pip install -r birmingham_cabinet/requirements.dev.txt
 fi
 
 cd redundancy_payments_alpha
