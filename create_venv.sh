7766c72eaf9c4e14552b73c71285443606d48f6e
Jim Gumbley <jgumbley@thoughtworks.com>
2013-11-08 11:05:47 +0000

    rename script to more descriptive name

---
+++ create_venv.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+VENVNAME=rps
+
+if [ -z "$VIRTUAL_ENV" -a -n "$WORKON_HOME" ]; then
+
+   venvdir=$WORKON_HOME/$VENVNAME
+
+   if [ ! -d "$venvdir" ]; then
+     virtualenv $venvdir
+   fi
+
+   source "$venvdir/bin/activate"
+
+   # meh - see https://github.com/pypa/pip/issues/56
+   pip install -r redundancy_payments_alpha/requirements.dev.txt
+   pip install -r birmingham_cabinet/requirements.dev.txt
+
+   cd birmingham_cabinet
+   ./ensure_clean_tables
+   cd ..
+fi
