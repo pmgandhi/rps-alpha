e677c23739f6f6df5c9f01634d74f69e45d381da
Jim Gumbley <jgumbley@thoughtworks.com>
2013-11-09 14:25:30 +0000

    move the get_xml_value func into helper module

---
+++ redundancy_payments_alpha/chomp/unit_tests/helpers.py
@@ -0,0 +1,13 @@
+from xml.dom.minidom import parseString
+from xpath import findvalues
+
+
+def get_value_from_xpath(xpath_sel, xml_doc):
+    """This is a test helper method which gets an attribute name and returns
+    its value from an xml fragment using xpath.
+    """
+    xml_dom = parseString(xml_doc)
+    try:
+        return findvalues(xpath_sel, xml_dom)[0]
+    except IndexError:
+        raise Exception("%s not found in xml_document" % xpath_sel)
