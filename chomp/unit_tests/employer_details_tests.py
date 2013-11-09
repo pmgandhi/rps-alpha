e39440b3edd2e242354b248f7b375c3bbbe850bf
Jim Gumbley <jgumbley@thoughtworks.com>
2013-11-09 06:40:17 +0000

    further refactoring of tests

--- chomp/unit_tests/employer_details_tests.py
+++ chomp/unit_tests/employer_details_tests.py
@@ -1,4 +1,4 @@
-from hamcrest import assert_that, is_, has_item
+from hamcrest import assert_that, is_, has_item, none
 from bs4 import BeautifulSoup
 import json
 
@@ -10,10 +10,9 @@ EMPLOYER_DETAILS_JSON = """
     "employer_details": {
         "company_name": "Scrooge McDuck Enterprises",
         "date_of_insolvency": "23/13/2013",
-        "company_number": "whatever",
-        "nature_of_business": "whatever",
-        "type_of_insolvency": "whatever",
-        "date_of_insolvency": "whatever",
+        "company_number": "not-in-minimum",
+        "nature_of_business": "not-in-minimum",
+        "type_of_insolvency": "Creditors Voluntary Liquidation",
         "insolvency_practitioner_name": "whatever",
         "insolvency_practitioner_registration": "whatever",
         "insolvency_practitioner_firm": "whatever",
@@ -34,7 +33,11 @@ def get_xml_attribute_from(xml_fragment, attr_name):
     content from an xml fragment.
     """
     xml_soup = BeautifulSoup(xml_fragment, "xml")
-    return xml_soup.find(attr_name).contents[0]
+    get_from_attr = xml_soup.find(attr_name)
+    if get_from_attr:
+        return get_from_attr.contents[0]
+    else:
+        return None
 
 def check_json_is_mapped_into_xml(xml_attribute, json_attribute ):
     """Assertions on the xml payload to ensure that the json value has been populated
@@ -47,6 +50,7 @@ def check_json_is_mapped_into_xml(xml_attribute, json_attribute ):
     xml_payload = generate_rp14_request(employer_json)
     # then
     assert_that(employer_json, has_item(json_attribute) )
+    assert_that(get_xml_attribute_from(xml_payload, xml_attribute), not none() )
     assert_that(employer_json[json_attribute], is_(get_xml_attribute_from(xml_payload, xml_attribute)))
 
 def test_claimant_information_json_is_mapped_to_valid_champ_xml():
@@ -56,6 +60,8 @@ def test_claimant_information_json_is_mapped_to_valid_champ_xml():
     field_mapping = {
         "company_name" : "NameOfBusiness",
         "date_of_insolvency" : "InsolvencyDate",
+        "type_of_insolvency" : "InsolvencyType",
+        "insolvency_practitioner_name" : "Name"
     }
     # test that
     for json_attribute, xml_attribute in field_mapping.iteritems():
