8549783bb9e3aab7db884f6ad79e373009d722b3
Leena Gupte <leena.gupte@gmail.com>
2013-11-12 14:21:54 +0000

    Change styling of employee_record template

--- redundancy_payments_alpha/unit_tests/claimant_contact_details/test_matching_claimant_and_employee_details.py
+++ redundancy_payments_alpha/unit_tests/claimant_contact_details/test_matching_claimant_and_employee_details.py
@@ -9,12 +9,12 @@ class TestMatchingClaimantAndEmployeeDetails(unittest.TestCase):
         response = test_app.get('/claim-redundancy-payment/employee-records/?nino=AB999999C')
         assert_that(response.status_code, is_(200))
         page = BeautifulSoup(response.data)
-        assert_that(page.find('h1').text, contains_string('Employee Record Not Found'))
+        assert_that(page.find('h1').text, contains_string('No Employee Record Found'))
 
     def test_claimant_is_shown_employee_record_for_nino(self):
         test_app = routes.app.test_client()
         response = test_app.get('/claim-redundancy-payment/employee-records/?nino=AB111111C')
         assert_that(response.status_code, is_(200))
         page = BeautifulSoup(response.data)
-        assert_that(page.find('h1').text, contains_string('Employee Record Found'))
+        assert_that(page.find('h1').text, contains_string('Your Employee Record'))
         assert_that(page.find('pre').text, contains_string('John'))
